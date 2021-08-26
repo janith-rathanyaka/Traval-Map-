64307b0f49bfee7bbcdae10c595cb7a4019d8b16
janith-rathnayaka <mnjrathnayaka97@yahoo.com>
2021-08-26 15:28:37 +0530

    update frontent Map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import ReactMapGL,{Marker} from 'react-map-gl';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 37.78,
+    longitude: -122.45,
+    zoom: 4
+  });
   return (
     <div className="App">
+       <ReactMapGL
+       {...viewport}
+       mapboxApiAccessToken={'pk.eyJ1IjoiamFuaXRoMTIzIiwiYSI6ImNrc3NwcW1lbjA4Zjkydm5zYXN3OXRtN2IifQ.IvKAludDmrZbfH_W7EVrbQ'}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
       
+    >
+      <Marker
+       latitude={37.78}
+       longitude={-122.41}
+       offsetLeft={-20} 
+       offsetTop={-10}
+      >
+        <div>you are here</div>
+
+      </Marker>
+
+    </ReactMapGL>
     </div>
   );
 }
